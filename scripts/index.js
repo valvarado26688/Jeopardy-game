95104510a722c19978009a1c3c22d8fe10fddd03
Victor Alvarado <valvarado26688@gmail.com>
2024-09-11 21:53:25 -0400

    experimenting with how to manipulate data from "placholder-questions" (i forgot)

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
 // Do not change the import statement
 import placeholderQuestions from "./placeholder-questions.js";
 console.log({ placeholderQuestions });
+console.log(placeholderQuestions.category);
+console.log(placeholderQuestions[0]);
+// console.log(placeholderQuestions.includes(category, "nature"));
+let cat = placeholderQuestions.filter(() => placeholderQuestions.category === "Nature");
+console.log(cat);
+
+placeholderQuestions.forEach((e) => {
+    if (e.category === "Nature") {
+        console.log(e);
+    }
+})
+
+
