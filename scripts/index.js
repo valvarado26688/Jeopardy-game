141b5d56aa48e8fb3fa9c42ce0716974b345a18b
Victor Alvarado <valvarado26688@gmail.com>
2024-09-11 23:36:00 -0400

    mapped all questions into an array(might be on to something)

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,24 @@
 // Do not change the import statement
 import placeholderQuestions from "./placeholder-questions.js";
 console.log({ placeholderQuestions });
-console.log(placeholderQuestions.category);
+
+
+
+// console.log(placeholderQuestions.category);
 console.log(placeholderQuestions[0]);
 // console.log(placeholderQuestions.includes(category, "nature"));
 let cat = placeholderQuestions.filter(() => placeholderQuestions.category === "Nature");
 console.log(cat);
 
 placeholderQuestions.forEach((e) => {
-    if (e.category === "Nature") {
-        console.log(e);
-    }
+    console.log(e.category);
 })
+addEventListener("click", () => {});
 
+
+
+let questions = placeholderQuestions.map((e) => {
+    return e.question
+});
+console.log(questions);
 
