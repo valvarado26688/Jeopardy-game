81ad067f531b235e341649daa0e1ddf9d3741037
Victor Alvarado <valvarado26688@gmail.com>
2024-09-16 18:55:38 -0400

    attempt making class to sort questions

--- scripts/index.js
+++ scripts/index.js
@@ -5,20 +5,50 @@ console.log({ placeholderQuestions });
 
 
 // console.log(placeholderQuestions.category);
-console.log(placeholderQuestions[0]);
+// console.log(placeholderQuestions[0]);
 // console.log(placeholderQuestions.includes(category, "nature"));
-let cat = placeholderQuestions.filter(() => placeholderQuestions.category === "Nature");
-console.log(cat);
+// let cat = placeholderQuestions.filter(() => placeholderQuestions.category === "Nature");
+// console.log(cat);
 
-placeholderQuestions.forEach((e) => {
-    console.log(e.category);
-})
-addEventListener("click", () => {});
+// placeholderQuestions.forEach((e) => {
+//     console.log(e.category);
+// })
+
+// let q100 = document.getElementById("q100");
 
 
 
-let questions = placeholderQuestions.map((e) => {
-    return e.question
-});
-console.log(questions);
 
+//this function stores all the FIRST questions of every catagory into an array
+//not sure if this would be usefull
+function firstQuestion(placeholderQuestions){
+    const firstQuestions = {};
+
+    placeholderQuestions.forEach((e) => {
+        if(!firstQuestions[e.category]) {
+            firstQuestions[e.category] = e.question;
+        }
+    });
+
+    return firstQuestions;
+}
+// let questionsByCategory = firstQuestion(placeholderQuestions);
+// console.log(questionsByCategory); 
+
+
+//hopefully this will give me everything...
+//broken at the moment.
+class QuestionMachine5000 {
+    constructor(questions) {
+        this.question = questions;
+    }
+    questionsByCat(category) {
+        return this.question.filter((q) => {
+            q.category === category;
+        })
+    }
+}
+
+let questions = new QuestionMachine5000(placeholderQuestions)
+
+console.log(questions.questionsByCat(`Nature`));
