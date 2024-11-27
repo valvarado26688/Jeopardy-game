99fb2defdb4162aed1e728e3cb810351686d2ed2
Victor Alvarado <valvarado26688@gmail.com>
2024-11-27 00:30:08 -0500

    sorted questions easy peasy

--- scripts/index.js
+++ scripts/index.js
@@ -1,114 +1,44 @@
-// Do not change the import statement
 import placeholderQuestions from "./placeholder-questions.js";
 
+//consider making the landing page with js dom manipulation for a smooth user experience
+//game might need to be in a loop?
 
-//==========tests====================
-//this function stores all the FIRST questions of every catagory into an array
-//not sure if this would be usefull
-// function firstQuestion(placeholderQuestions){
-//     const firstQuestions = {};
+//sort placeholder questions 
+//keep in mind that placeholder questions is an array of objects, hence it could be sorted iterativly 
+//make two arrays of objects, 5 questions forEach category
 
-//     placeholderQuestions.forEach((e, i) => {
-//         if(!firstQuestions[e.category]) {
-//             firstQuestions[e.category] = e.question;
-//         }
-//     });
+//once questions are sorted, use the array to make a dynamic chart containing each question
 
-//     return firstQuestions;
-// }
-// class QuestionMachine5000 {
-//     constructor(questions) {
-//         this.questions = questions;
-//     }
-//     questionsByCat(category) {//doesnt seem like this is usefull but its neat 
-//         return this.questions.filter(q => q.category === category); 
-//     }
-
-//     //method helps get any specific question easily.
-//     //filters category uses num as an index number(starting from 0) 
-//     questionsByCatNum(category, num){
-//         let question = this.questionsByCat(category);
-//         return question[num];
-//     }
-//     // firstFiveQuestions(){
-//     //    let questions = {};
-
-//     //    for(const e of this.questions) {
-
-//     //    }
-//     // }
-// }
-// let sortedQuestions = new QuestionMachine5000(placeholderQuestions);
-//==========================================================
+//isolating last question
+console.log(placeholderQuestions);
+const finalQuestion = placeholderQuestions.pop();
+console.log(finalQuestion);
 
 
-
-
-
-
-
-let questionPoints = 100
-let gridContainer = document.getElementById("gridContainer");
-let index = 0
-//code to make parent elements 
-for(let i = 0; i < 6; i++) {
-    let parentColumn = document.createElement("div");
-    gridContainer.append(parentColumn);
-    parentColumn.className = "parentColumn";
-
-
-    //creates and assign elements with point, then appends them as a child to parentColumn
-    for(let i = 0; i < 5; i++){
-        let div = document.createElement("div");
-        div.textContent = questionPoints;
-        div.className = "divColumn";
-        parentColumn.appendChild(div);
-        questionPoints = questionPoints + 100;
-        if(questionPoints === 600) {
-            questionPoints = 100;
-        }
-
-        //this is where the fun begins
-        div.addEventListener("click", () => {
-            div.textContent = firstQuestionIndex(index);
-            index++;
-        })
-        //this almosts does its job, it only however questions are put down in the exact order that you click the cells
-        //in fact if you keep clicking the same box, it goes through all the questions??????
+// counter < 5 = first questions; counter > 5 = second questions
+//this is the easiest way i can come up with on my own, and understand
+let counter = -1;
+const firstRoundQuestions = placeholderQuestions.map((e) => {
+    counter++;
+    if(counter === 10) {
+        counter = 0;
     }
-}
+    if(counter < 5) {
+        return e;
+    }
+    return null;
+}).filter((e) => e !== null);
+console.log(firstRoundQuestions);
 
-//lots of work to do, lots of things to learn. 
-
-
-
-//function creates an array of the first five questions of every category 
-//I chose to have the function to hopefully improve readability of my code above. 
-//I plan to use "index" as a way to easily return questions iteratively. 
-function firstQuestionIndex(index){
-    
-    let questions = {};//object to hold my categories
-
-
-
-    placeholderQuestions.forEach((e) => {
-
-        //creates a new key for every category. 
-        let category = e.category;
-        if(!questions[category]) {//does the key NOT exist?
-            questions[category] = [];//make one 
-        }
-
-        if(questions[category].length < 5 ) { 
-            questions[category].push(e);
-        }
-    })
-    console.log(questions);
-    let indexQuestions = Object.values(questions).flat();//it just works
-    console.log(indexQuestions);
-    // console.log(indexQuestions[0].question);
-
-    return indexQuestions[index].question;
-}
-
-// console.log(firstQuestionIndex(5));
+counter = -1
+const secondRoundQuestions = placeholderQuestions.map((e) => {
+    counter++;
+    if(counter === 10) {
+        counter = 0;
+    }
+    if(counter >= 5) {
+        return e;
+    }
+    return null;
+}).filter((e) => e !== null);
+console.log(secondRoundQuestions);
