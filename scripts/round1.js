0467daf05a10f6c649ed1f692b5e45b7b826feba
Victor Alvarado <valvarado26688@gmail.com>
2025-01-08 00:23:43 -0500

    event listeners disable until submit, score kinda kept track

--- scripts/round1.js
+++ scripts/round1.js
@@ -26,10 +26,6 @@ export default function round1(q) {
     }
 
     //game chart
-    //new goal: chart will only display the chart of numbers.
-    //once clicked on, box below the chart will display the question
-    //wether or not the question is answered correctly; the number will remain blank
-    //score must be kept in a variable, function return score to index.js 
     let gameChart = document.createElement("div");
     document.body.append(gameChart);
     gameChart.classList.add("gameChart");
@@ -42,14 +38,31 @@ export default function round1(q) {
     let answerInput = document.createElement("input");
     answerInput.classList.add("answerInput");
     answerInput.placeholder = "answer here";
+    gameChart.parentElement.appendChild(answerInput);
 
     let submitButton = document.createElement("button");
     submitButton.classList.add("submitButton");
     submitButton.textContent = "submit";
+    gameChart.parentElement.appendChild(submitButton);
 
     let playerState = 1;
     let player1Score = 0;
     let player2Score = 0;
+
+    let scoreContainer = document.createElement("div");
+    scoreContainer.classList.add("scoreContainer");
+    gameChart.parentElement.appendChild(scoreContainer);
+
+    let displayScore1 = document.createElement("div");
+    displayScore1.classList.add("displayScore1");
+    scoreContainer.appendChild(displayScore1);
+    displayScore1.textContent = "player 1 score: " + player1Score;
+
+    let displayScore2 = document.createElement("div");
+    displayScore2.classList.add("displayScore2");
+    scoreContainer.appendChild(displayScore2);
+    displayScore2.textContent = "player 2 score: " + player2Score;
+
     let count = 0;
     for(let i = 0; i < 6; i++) {
         let columnContainer = document.createElement("div");
@@ -67,34 +80,45 @@ export default function round1(q) {
             column.addEventListener("click", () => {
                 column.textContent = "";
                 questionBox.textContent = question;
-                gameChart.parentElement.appendChild(answerInput); //questions should display after click
-                gameChart.parentElement.appendChild(submitButton);
+
+                for(let child of gameChart.children) {
+                    child.style.pointerEvents = "none";
+                }
 
                 //hold up im cooking
                 submitButton.onclick = () => {
                     let userAnswer = answerInput.value.trim().toLowerCase();
                     console.log(userAnswer);
 
-                    //answers are functional, now is time to make this more dynamic
                     console.log(playerState);
                     if(playerState === 1) {
                         if(userAnswer === answer.trim().toLowerCase()) {
                             player1Score += i*100;
                             playerState = 2;
                             playerTurn.textContent = "PLAYER 2 TURN";
+                            displayScore1.textContent = "player 1 score: " + player1Score;
                         } else {
                             console.log("incorrect"); //this will be placed somewhere on the question box
                             playerState = 2;
+                            playerTurn.textContent = "PLAYER 2 TURN";
                         }
                     } else if(playerState === 2) {
                         if(userAnswer === answer.trim().toLowerCase()) {
                             player2Score += i*100;
                             playerState = 1;
                             playerTurn.textContent = "PLAYER 1 TURN";
+                            displayScore2.textContent = "player 2 score: " + player2Score;
                         } else {
                             console.log("incorrect");
+                            playerState = 1;
+                            playerTurn.textContent = "PLAYER 1 TURN";
                         }
                     }
+
+                    for(let child of gameChart.children) { //worked the first time
+                        child.style.pointerEvents = "auto";
+                    }
+                    //NOTE: am considering making the function return the player scores so they can be used in index.js
                 }
             })
             count++;
